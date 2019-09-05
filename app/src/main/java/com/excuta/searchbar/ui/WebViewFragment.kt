b3ecb56f0f589269756129058468ae6e59c9431e
Excuta <ymr_15@live.com>
2019-09-05 23:33:14 +0200

    implement search in toolbar

--- app/src/main/java/com/excuta/searchbar/ui/WebViewFragment.kt
+++ app/src/main/java/com/excuta/searchbar/ui/WebViewFragment.kt
@@ -1,18 +1,35 @@
 package com.excuta.searchbar.ui
 
-
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 
 import com.excuta.searchbar.R
+import com.excuta.searchbar.ui.extensions.hideKeyboard
+import io.reactivex.Emitter
+import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.ObservableOnSubscribe
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Predicate
+import kotlinx.android.synthetic.main.fragment_web_view.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class WebViewFragment : Fragment() {
+
+    private lateinit var searchView: SearchView
+    private var disposable: Disposable? = null
+    private val queryLiveData = MutableLiveData<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +39,34 @@ class WebViewFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_web_view, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        queryLiveData.observe(viewLifecycleOwner, Observer { text.text = it })
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        searchView = searchItem.actionView as SearchView
+        disposable?.dispose()
+        disposable = Observable.create(ObservableOnSubscribe<String> {
+            searchView.setOnQueryTextListener(createQueryListener(it))
+        })
+            .filter { !it.isBlank() }
+            .subscribe { queryLiveData.postValue(it) }
+    }
+
+    private fun createQueryListener(emitter: Emitter<String>): SearchView.OnQueryTextListener {
+        return object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) emitter.onNext(query)
+                hideKeyboard()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/excuta/searchbar/ui/extensions/FragmentExtensions.kt
@@ -0,0 +1,11 @@
+package com.excuta.searchbar.ui.extensions
+
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import androidx.fragment.app.Fragment
+
+fun Fragment.hideKeyboard() {
+    val systemService =
+        this.context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    systemService.hideSoftInputFromWindow(this.view?.windowToken, 0)
+}
